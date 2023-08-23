101819420971cf3c55789d4d7d868f7527e03a6c
Kenneth032 <142145067+Kenneth032@users.noreply.github.com>
2023-08-23 20:52:14 +0300

    Added routing to Speaker Profile Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,46 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:qfapp/pages/Contact.dart';
-import 'package:qfapp/pages/home.dart';
-import 'package:qfapp/pages/Services.dart';
-import 'package:qfapp/pages/About.dart';
-import 'package:qfapp/pages/BookingForm.dart';
-import 'package:qfapp/pages/BusinessPlanConsultService.dart';
-import 'package:qfapp/pages/CateringService.dart';
-import 'package:qfapp/pages/CoachingService.dart';
-import 'package:qfapp/pages/EventManagementService.dart';
-import 'package:qfapp/pages/InternshipService.dart';
-import 'package:qfapp/pages/Workshops.dart';
+import 'package:flutter_alqoud/Contact.dart';
+import 'package:flutter_alqoud/home.dart';
+import 'package:flutter_alqoud/Services.dart';
+import 'package:flutter_alqoud/About.dart';
+import 'package:flutter_alqoud/BookingForm.dart';
+import 'package:flutter_alqoud/BusinessPlanConsultService.dart';
+import 'package:flutter_alqoud/CateringService.dart';
+import 'package:flutter_alqoud/CoachingService.dart';
+import 'package:flutter_alqoud/EventManagementService.dart';
+import 'package:flutter_alqoud/InternshipService.dart';
+import 'package:flutter_alqoud/Workshops.dart';
+import 'package:flutter_alqoud/SpeakerProfile.dart';
 
 void main() {
-      runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-      const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-      @override
-      Widget build(BuildContext context) {
-            return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  initialRoute: '/',
-                  routes: {
-                        '/': (context) => Home(),
-                        "/Services": (context) => Services(),
-                        "/About": (context) => About(),
-                        "/Contact": (context) => Contact(),
-                        "/BookingForm": (context) => BookingForm(),
-                        "/BusinessPlanConsultService": (context) => BusinessPlanConsultService(),
-                        "/CateringService": (context) => CateringService(),
-                        "/CoachingService": (context) => CoachingService(),
-                        "/EventManagementService": (context) => EventManagementService(),
-                        "/InternshipService": (context) => InternshipService(),
-                        "/Workshops": (context) => Workshops(),
-                  },
-            );
-      }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        "/Services": (context) => Services(),
+        "/About": (context) => About(),
+        "/Contact": (context) => Contact(),
+        "/BookingForm": (context) => BookingForm(),
+        "/BusinessPlanConsultService": (context) =>
+            BusinessPlanConsultService(),
+        "/CateringService": (context) => CateringService(),
+        "/CoachingService": (context) => CoachingService(),
+        "/EventManagementService": (context) => EventManagementService(),
+        "/InternshipService": (context) => InternshipService(),
+        "/Workshops": (context) => Workshops(),
+        "/SpeakerProfile": (context) => SpeakerProfile(),
+      },
+    );
+  }
 }
