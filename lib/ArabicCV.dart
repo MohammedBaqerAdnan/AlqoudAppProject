dbdb7f0e0e1bcde5f191fda8b5ca487fa71d3925
Kenneth032 <142145067+Kenneth032@users.noreply.github.com>
2023-08-24 19:03:42 +0300

    Created Arabic and English CV pages
    Temporarily using a sample CV as a placeholder

---
+++ lib/ArabicCV.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class ArabicCV extends StatelessWidget {
+  ArabicCV({Key? key}) : super(key: key);
+
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        key: _globalKey,
+        drawer: Drawer(
+          child: Container(
+            color: Colors.grey[300],
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("name"),
+                  accountEmail: Text("email"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.grey[200],
+                    child: Text(
+                      "A",
+                      style: TextStyle(fontSize: 40.0),
+                    ),
+                  ),
+                  decoration: BoxDecoration(color: Colors.grey[900]),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Home"),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text("About"),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/About');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Services"),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/Services');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.contacts),
+                  title: Text("Contact Us"),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/Contact');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text('Speaker Arabic CV'),
+          centerTitle: true,
+          backgroundColor: Colors.amber[500],
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+        ),
+        //pdf viewer using pdf asset
+        body: SfPdfViewer.asset('asset/SampleCV.pdf'),
+      ),
+    );
+  }
+}
